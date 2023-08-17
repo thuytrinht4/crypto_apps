7cd9f435073e4591dbd1ebc8c7e9ecd8117df0b2
Thuy Trinh <thuytrinht4@gmail.com>
2023-08-17 18:23:10 +0100

    try user login

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+# from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
 
+# Mock user database (replace with a real database)
+users = {'test1': '123456'}
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def home():
+    return render_template('login.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    if username in users and users[username] == password:
+        return 'Login successful!'
+    else:
+        return 'Login failed. Please try again.'
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, world!'

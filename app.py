46e5321bb097c1280586f7ee10b6c8a1c1464422
Thuy Trinh <thuytrinht4@gmail.com>
2023-08-18 13:14:37 +0100

    add portfolio value and format display

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# from flask import Flask
+import locale
 from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 
 # Set a temporary secret key for testing
 app.secret_key = 'temporary_test_key'
+
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+
+@app.template_filter('format_currency')
+def format_currency(value, decimal_num=0):
+    formatted_value = locale.format_string(
+        f"%.{decimal_num}f", value, grouping=True)
+    return formatted_value
+
 
 # Mock user database (replace with a real database)
 dummy_users = {
@@ -43,6 +53,10 @@ def dashboard():
         'Crypto User 1': {
             'user_name': 'John Dow',
             'email': 'crypto_user1@gemini_test.com',
+            'eft': {
+                'etf_name': 'ETF-5',
+                'etf_detail': 'Portfolio with Top 5 Cryptocurrencies'},
+            'total_fund': 10000,
             'portfolio': [
                 {'cryptocurrency': 'Bitcoin', 'amount_owned': 0.2485,
                  'current_price': 26500, 'value': 6585},
@@ -60,6 +74,10 @@ def dashboard():
         'Crypto User 2': {
             'user_name': 'Tuan Trinh',
             'email': 'crypto_user1@gemini_test.com',
+            'eft': {
+                'etf_name': 'ETF-2',
+                'etf_detail': 'Portfolio with Top 2 Cryptocurrencies'},
+            'total_fund': 10000,
             'portfolio': [
                 {'cryptocurrency': 'Bitcoin', 'amount_owned': 0.2726,
                  'current_price': 26500, 'value': 7223},
@@ -75,6 +93,7 @@ def dashboard():
         user_id = session['user_id']
         user_data = dummy_user_data.get(user_id)
         if user_data:
+            session['total_fund'] = user_data['total_fund']
             return render_template('dashboard.html', user_data=user_data)
         else:
             return "User not found"
